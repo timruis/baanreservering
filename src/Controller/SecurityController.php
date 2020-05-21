1372d64c34dbabe9962d8f86658df9312115ad02
timruis <38651429+timruis@users.noreply.github.com>
2020-05-21 18:12:14 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -60,6 +60,7 @@ class SecurityController extends AbstractController
             $User->setFirstname($data->getFirstname());
             $User->setLastname($data->getLastname());
             $User->setActivateUser(false);
+            $User->setPayed(false);
             $em->persist($User);
             $em->flush();
             return $this->redirectToRoute('app_login');
